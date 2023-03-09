9b25abfffc6a1409b5cb1313a018bdbe8add7801
jeremygradisher <jeremygradisher@gmail.com>
2023-03-09 16:19:08 -0500

    Rendered some JSX saved within a variable

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Write code here:
+const myList = (
+  <ul>
+    <li>Just showing how to pass JSX</li>
+    <li>as a variable</li>
+    <li>This is the way</li>
+  </ul>
+);
+
 // Copy code here:
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
+//ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
+ReactDOM.render(myList, document.getElementById('root'));
